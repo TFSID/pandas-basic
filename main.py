1ad7609f8fe7784ed6a8e9b8800997537d8d4242
TFSID <93431570+TFSID@users.noreply.github.com>
2025-09-17 09:49:47 +0700

    update main

--- main.py
+++ main.py
@@ -7,21 +7,13 @@ def open1():
         data = file.readlines()
         print(data)
 
-
-# buka data "Kerentanan Asset Internal.csv" dengan open()
-
-# with open('Kerentanan Asset Internal.csv', 'r') as file:
-    # pd.read_csv(file)
-    # data = file.readlines()
-    # print(data)
-
 def open2():
     with open('Kerentanan Asset Internal.csv', 'r') as file:
         df = pd.read_csv(file)
         print(df)
 
 def open3():
-    file_csv = 'Kerentanan Asset Internal.csv'
+    file_csv = 'data/Kerentanan Asset Internal.csv'
     df = pd.read_csv(file_csv)
     print(df.columns)
 open3()
